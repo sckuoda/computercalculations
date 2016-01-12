b74536870866592cfdf9f4b40df29da034e48d2a
Samantha Kuo <samantha.kuo@gmail.com>
2016-01-11 23:13:41 -0500

    Commit Sam's project

---
+++ src/calculatingcomputer/Computercalculation2.java
@@ -0,0 +1,220 @@
+package calculatingcomputer;
+
+public class Computercalculation2 {
+
+	private String mouse;
+    private double cost;
+    private double time;
+    private String laptop;
+    private int size;
+    private double awake;
+    private String cpu;
+    private String disk;
+    private String mb;
+    private String country;
+    private double countryElectricityAverage;
+    private double electricityUse;
+    private double totalElectricityCost;
+    private double totalBatteryCost;
+    
+    
+    public double getTotalBatteryCost() {
+		return totalBatteryCost;
+	}
+
+
+
+	public void setTotalBatteryCost(double totalBatteryCost) {
+		this.totalBatteryCost = totalBatteryCost;
+	}
+
+
+
+	private double totalComputerCost;
+    private double mice;
+    
+    public double getElectricityUse() {
+		return electricityUse;
+	}
+
+
+
+	public void setElectricityUse(double electricityUse) {
+		this.electricityUse = electricityUse;
+	}
+
+
+
+	public String getLaptop() {
+		return laptop;
+	}
+
+
+
+	public void setLaptop(String laptop) {
+		this.laptop = laptop;
+	}
+
+
+
+	public int getSize() {
+		return size;
+	}
+
+
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+
+
+	public double getTime() {
+		return time;
+	}
+
+
+
+	public void setTime(double time) {
+		this.time = time;
+	}
+
+
+
+	public Computercalculation2(){
+    	
+    }
+
+
+
+	public double getCost() {
+		return cost;
+	}
+
+
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+
+
+	public String getMouse() {
+		return mouse;
+	}
+
+
+
+	public void setMouse(String mouse) {
+		this.mouse = mouse;
+	}
+
+
+
+	public double getawake() {
+		return awake;
+	}
+
+
+
+	public void setawake(double awake) {
+		this.awake = awake;
+	}
+
+
+
+	public String getcpu() {
+		return cpu;
+	}
+
+
+
+	public void setcpu(String cpu) {
+		this.cpu = cpu;
+	}
+
+
+
+	public String getdisk() {
+		return disk;
+	}
+
+
+
+	public void setdisk(String disk) {
+		this.disk = disk;
+	}
+
+
+
+	public String getmb() {
+		return mb;
+	}
+
+
+
+	public void setmb(String mb) {
+		this.mb = mb;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+
+	public double getCountryElectricityAverage() {
+		
+		return countryElectricityAverage;
+	}
+
+
+
+	public void setCountryElectricityAverage(double countryElectricityAverage) {
+		this.countryElectricityAverage = countryElectricityAverage;
+	}
+	
+	
+	public double getTotalElectricityCost() {
+		return totalElectricityCost;
+	}
+
+
+
+	public void setTotalElectricityCost(double totalElectricityCost) {
+		this.totalElectricityCost = totalElectricityCost;
+	}
+
+
+
+	public double getTotalComputerCost() {
+		return totalComputerCost;
+	}
+
+
+
+	public void setTotalComputerCost(double totalComputerCost) {
+		this.totalComputerCost = totalComputerCost;
+	}
+
+
+
+	public double getMice() {
+		return mice;
+	}
+
+
+
+	public void setMice(double mice) {
+		this.mice = mice;
+	}
+	
+
+}
